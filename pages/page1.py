4f7e83bc38c2f5eb68da808cb721039f1a677e07
Bunbu- <kanrapapurukku@icloud.com>
2025-08-03 11:27:13 +0900

    ユーザー情報表示のプログラムを作りました。

--- pages/page1.py
+++ pages/page1.py
@@ -1,3 +1,13 @@
 import streamlit as st
 
-st.write("おはよう")
+st.title("👤ユーザー情報表示ページ")
+
+if 'user_name' in st.session_state and st.session_state.user_name:
+    st.success(f"🎉こんにちは、{st.session_state.user_name}さん！")
+    st.write("メインページで入力された名前が正しく表示されています。")
+
+    st.balloons()
+
+else:
+    st.error("✖ユーザ名が設定されていません")
+    st.write("メインページで名前を入力してください")
