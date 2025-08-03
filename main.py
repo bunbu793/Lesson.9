22c05bb9d76192fd013a4151b0d84d1c42863464
Bunbu- <kanrapapurukku@icloud.com>
2025-08-03 10:27:05 +0900

    名前を記録できるプログラムを打ちました。

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Helloword")
+import streamlit as st
+
+st.title("名前記憶アプリ")
+
+if 'you' not in st.session_state:
+    st.session_state.you =""
+
+name = st.text_input("あなたの名前を入力してください")
+if st.button("名前を記録"):
+    st.session_state.you = name
+
+st.write(f"記憶している名前:{st.session_state.you}")
