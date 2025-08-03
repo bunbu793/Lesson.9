ccc7ae83cd6a094ae6114086c66ea230b584b457
Bunbu- <kanrapapurukku@icloud.com>
2025-08-03 10:37:04 +0900

    ひとつ前のプログラムの変数名がyouになっていたため、givennameにしました。

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import streamlit as st
 
 st.title("名前記憶アプリ")
 
-if 'you' not in st.session_state:
-    st.session_state.you =""
+if 'givenname' not in st.session_state:
+    st.session_state.givenname =""
 
 name = st.text_input("あなたの名前を入力してください")
 if st.button("名前を記録"):
-    st.session_state.you = name
+    st.session_state.givenname = name
 
-st.write(f"記憶している名前:{st.session_state.you}")
+st.write(f"記憶している名前:{st.session_state.givenname}")
